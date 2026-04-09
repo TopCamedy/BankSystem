20dc28c47340a1e7bca8c3177b528f7eb37ac816
TopCamedy <denchikslpchv@gmail.com>
2026-04-09 14:08:55 +0300

    Первая часть задания, реализовал классы

---
+++ П1/AbstractAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace BankSystem
+{
+    public abstract class AbstractAccount
+    {
+        public string AccountId; // Уникальный номер
+        public string OwnerName; // ФИО владельца
+        public decimal Balance;  // Остаток на счету
+        public AccountStatus Status;
+
+        public AbstractAccount(string ownerName)
+        {
+            // Генерируем случайный номер при создании
+            AccountId = "ID" + new System.Random().Next(1000, 9999);
+            OwnerName = ownerName;
+            Balance = 0;
+            Status = AccountStatus.Active;
+        }
+
+        // virtual позволяет дочерним классам менять логику этих методов
+        public virtual void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public virtual void Withdraw(decimal amount)
+        {
+            if (Balance >= amount) Balance -= amount;
+        }
+    }
+}
---
+++ П1/Common.cs
@@ -0,0 +1,7 @@
+﻿namespace BankSystem
+{
+    // Список состояний счета
+    public enum AccountStatus { Active, Frozen, Closed }
+    // Список доступных валют
+    public enum Currency { RUB, USD, EUR }
+}
